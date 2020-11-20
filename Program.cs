eb91329ef9eb67a5c836aa93a578b8e198f1f9c6
Derlis <dsgezika@gmail.com>
2020-11-19 22:50:35 -0300

    nota

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+
+namespace Estruturas_de_repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+             do {
+                 Console.Write("Digite uma nota entre 0 e 10");
+                 nota = int.Parse(Console.ReadLine());
+
+             } while(!(nota >= 0 && nota <= 10));
+            //   while (nota < 0 || nota > 10);
+
+
+            Console.WriteLine("Número Aceito");
+        }
+    }
+}
